b0fd222a783f28794acc15a9b57f6986c8b56052
Fabricio-alt <santoscardoso370@gmail.com>
2020-06-08 22:13:54 -0300

    entrada de estoque

---
+++ src/main/App.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import './App.css'
+import React from 'react'
+
+//Importando componentes
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import Insumos from '../components/Insumos'
+
+export default props =>
+    <div className="app">
+        <Header />
+        <Nav />
+        <Insumos />
+    </div>
