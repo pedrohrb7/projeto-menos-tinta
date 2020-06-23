81019513acfd8412fc3b85547d889f7cb01d98cc
TiagoHeld <bondtiago@uol.com.br>
2020-06-23 20:39:25 -0300

    add login

--- src/_components/login/Login.jsx
+++ src/_components/login/Login.jsx
@@ -1,36 +1,72 @@
 import React, { Component } from 'react';
 
-// import inferior from '../assets/images/inferior.svg';
-// import logo from '../assets/images/logo.png';
-// import superior from '../assets/images/superior.svg';
-import photo from '../assets/images/photo.svg'
+
+import photo from '../../_imgs/photo.svg'
 import './Login.css';
 
 class Login extends Component {
-    render(){
-        return(
-        <>
-          <div className="container">
-              {/* <img src={inferior} className="gradienteInferior" /> */}
-           
-             {/* <img src={logo} className="logo" alt="Logo Menos Tinta" />    */}
-
-            <div className="box-login">
-                <img src={photo} className="photo" />
-                <label>Usuário</label>
-                <input type="text" placeholder="Usuário" required />
-                <label>Senha</label>
-                <input type="password" placeholder="Senha" />
-                <br />
-                <button>Acessar</button>
-                <br />
-                <a href="https://menostinta.com.br/" target="_blank">Não possui conta? Cadastre-se!</a>
-            </div>
+  constructor(props) {
+    super(props);
+    this.state={
+        email:'',
+        senha:'',
+        logado: false
+  };
+}	
+logar(event) {
+  event.preventDefault();
+  const { history } = this.props;
+  const {email,senha} = this.state;
+  var myHeaders = new Headers();
+  myHeaders.append("X-Requested-With", "XMLHttpRequest");
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+  
+  if(email === "" & senha==="") {
+    alert("Por favor digite um usuário e senha");
+  }
+  else {
+    fetch("https://cors-anywhere.herokuapp.com/54.159.114.209:211/datasnap/rest/TServerMethods1/Login?"+this.state.email+"&"+this.state.senha)
+            .then((response)=>response.json()).then((data)=>{ console.log(JSON.stringify(data))
+              if(JSON.stringify(data)==='') {
+               
+              } 
+              else {
+                 history.push('/PageControl'); 
+              }     
             
-            {/* <img src={superior} className="gradienteSuperior" />      */}
-          </div> 
-        </>         
-        )
-    }
+            
+            })
+            .catch((err)=>console.error(err))
+     
+  }          
+}
+render(){
+    return(
+
+      <div className="login">
+     
+        <div className="container-login">
+            
+
+          <form className="box-login" onSubmit={(e)=>this.logar(e)}>
+              <img src={photo} className="photo" />
+              <label>Usuário</label>
+              <input type="text" placeholder="Usuário" value={this.state.email} onChange={(e)=>this.setState({email:e.target.value})} required/>
+              <label>Senha</label>
+              <input type="password" placeholder="Senha" value={this.state.senha} onChange={(e)=>this.setState({senha:e.target.value})}/>
+              <br />
+              <button className="button-login">Acessar</button>
+              <br />
+              <a href="https://menostinta.com.br/" target="_blank">Não possui conta? Cadastre-se!</a>
+          </form>
+          
+       </div> 
+      </div>      
+    )
+  }
 }
 export default Login;
